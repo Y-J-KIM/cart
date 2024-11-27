b68dbfe2e5463a0635c37e4f474498b6ae448921
Y-J-KIM <dudwn717@naver.com>
2024-11-27 23:19:49 +0900

    히어로섹션 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HomePage from './components/Home/HomePage'
 import Navbar from './components/Navbar/Navbar'
 // import Routing from './components/Routing/Routing'
 
@@ -8,7 +9,9 @@ function App() {
   return (
     <div className='app'>
         <Navbar/>
-        <main>라우팅</main>
+        <main>
+          <HomePage />
+        </main>
     </div>
   )
 }
