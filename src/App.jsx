aa382130ed575d61e090bc27824530f90d08be23
Y-J-KIM <dudwn717@naver.com>
2024-11-27 23:03:31 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+// import Routing from './components/Routing/Routing'
+
+function App() {
+  
+
+  return (
+    <div className='app'>
+        <Navbar/>
+        <main>라우팅</main>
+    </div>
+  )
+}
+
+export default App
